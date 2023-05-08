8871df796b5c1546d9ac33b95a64d005b3359aab
krstnayou <95864385+krstnayou@users.noreply.github.com>
2023-05-08 07:42:14 -0400

    Add files via upload

--- v3/gem spa/sketch.js
+++ v3/gem spa/sketch.js
@@ -1,92 +1,152 @@
+// drawing ttf fonts with opentype.js and p5js commands.
+// note that this ONLY works with ttf fonts, which provide
+// the shapes of counters (i.e., the hole in 'O') in
+// counter-clockwise order. I use this in the code below
+// with the beginContour()/endContour() functions in p5js
+// to draw the shapes accordingly.
 
-let str = 'GEM SPA GEM SPA GEM SPA ';
-
-let startAngle =    0;     // angle where text should start
-let distanceAngle = 360;   // how far (in degrees) text will go
-
-let radius;                // set dynamically in setup()
 let font;
-let speedSlider, sizeSlider, numberSlider;
-let size;
+let fontData;
+var canvas;
 
+// groups a list of opentype.js vector commands by contour
+function groupByContour(cmds) {
+  contours = [];
+  current = [];
+  for (let cmd of cmds) {
+    current.push(cmd);
+    if (cmd.type == 'Z') {
+      contours.push(current);
+      current = [];
+    }
+  }
+  return contours;
+}
+
+// determines if a list of commands specify a path in clockwise
+// or counter-clockwise order
+function clockwise(cmds) {
+  let sum = 0;
+  for (let i = 0; i < cmds.length - 1; i++) {
+    let a = cmds[i];
+    let b = cmds[i+1];
+    if (!(a.hasOwnProperty('x') && b.hasOwnProperty('x'))) {
+      continue;
+    }
+    sum += (b.x - a.x) * (b.y + a.y);
+  }
+  return sum < 0;
+}
+
+// draws contours grouped by groupByContour(). uses clockwise()
+// to determine if this contour should be a p5js shape or a p5js
+// contour (i.e., cutout of a shape)
+function drawContours(contours) {
+  let inShape = false;
+  for (let i = 0; i < contours.length; i++) {
+    if (clockwise(contours[i])) {
+      if (inShape) {
+        endShape(CLOSE);
+      }
+      beginShape();
+      inShape = true;
+      drawContour(contours[i]);
+    }
+    else {
+      beginContour();
+      drawContour(contours[i]);
+      endContour();
+    }
+  }
+  if (inShape) {
+    endShape(CLOSE);
+  }
+}
+
+// draws an individual contour
+function drawContour(cmds) {
+  for (let i = 0; i < cmds.length; i++) {
+    cmd = cmds[i];
+    switch (cmd.type) {
+      case 'M':
+      case 'Z':
+        break;
+      case 'L':
+        vertex(cmd.x, cmd.y);
+        break;
+      case 'C':
+        bezierVertex(
+          cmd.x1, cmd.y1, cmd.x2, cmd.y2, cmd.x, cmd.y);
+        break;
+      case 'Q':
+        quadraticVertex(cmd.x1, cmd.y1, cmd.x, cmd.y);
+        break;
+		}    
+  }
+}
 
 function preload() {
-  font = loadFont('gemspa.otf');
+  fontData = loadBytes('GEMSPA.ttf');
 }
 
+let path;
 
 function setup() {
-  var cnv = createCanvas(windowWidth, windowHeight);
-  cnv.parent("footer");
-  radius = min(width,height) / 5;
-  
-  
-  speedSlider = createSlider(0,60,30);
-  speedSlider.position(1530,9550);
-  
-  sizeSlider = createSlider(-10,10,5);
-  sizeSlider.position(1530,9600);
-  
-  numberSlider = createSlider(-10,10,5);
-  numberSlider.position(1530,9650);
-
-  
+	canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position = (0,0);
+  font = opentype.parse(fontData.bytes.buffer);
+  path = font.getPath("GEM SPA", 0, 0, 72);
 }
 
+// applies a transformation to the x/y coordinates of each opentypejs
+// command you pass to it, according to the callback, which will be
+// given the x/y coordinates as parameters and should return an array
+// in the form of [x, y]
+function commandTransform(cmds, callback) {
+  let transformed = [];
+	for (let cmd of cmds) {
+    let newCmd = {type: cmd.type}
+    for (let pair of [['x', 'y'], ['x1', 'y1'], ['x2', 'y2']]) {
+      if (cmd.hasOwnProperty(pair[0]) && cmd.hasOwnProperty(pair[1])) {
+        let result = callback(cmd[pair[0]], cmd[pair[1]]);
+        newCmd[pair[0]] = result[0];
+        newCmd[pair[1]] = result[1];
+      }
+    }
+    transformed.push(newCmd);
+  }
+  return transformed;
+}
 
 function draw() {
-  
-  fill(255);
-  textFont("Space Mono")
-  textSize(18);
-  text('PRESS KEY TO DOWNLOAD',200,100);
-  
-  let s = speedSlider.value();
-  let size = sizeSlider.value();
+  background(128, 52, 52);
+  translate(mouseX, mouseY);
+  fill(40);
+  stroke(128);
 
-  textSize(size*10);
-  textFont(font);
-  textAlign(CENTER, BASELINE);
+//  clear();
+  fill(mouseX,100,100);
+noStroke();
+
+
   
-  
-  radius = min(width,height) / size;
-  
-  
-  background(100,100,mouseX,s*3);
-  frameRate(s);
-  
-  // the circle our text will go around
-  noFill();
-  noStroke();
-  circle(width/2,height/2, radius*2);
-  
-  // calculate the angle between each letter
-  let angleBetweenLetters = radians(distanceAngle) / str.length;
-  
-  // display the text!
   push();
-  translate(width/2, height/2);        // move to circle's center
-  rotate(radians(startAngle));         // rotate to where text starts
-  for (let i=0; i<str.length; i++) {   // go through each letter in the text
-    push();
-    rotate(i * angleBetweenLetters);   // rotate to angle
-    translate(0,-radius);              // and translate to edge of circle
-    fill(255);
-    noStroke();
-    text(str[i], 0,0);                 // draw character at location
-    pop();
-  }
+  translate(25, 250);
+  drawContours(
+    groupByContour(
+      commandTransform(path.commands, function(x, y) {
+        let newX, newY;
+        newY = y;
+        newX = x * map(cos(map(x, 0, 550, 0, TWO_PI)), -1, 1, 1,
+          map(sin(mouseX*0.01), -1, 1, 0.1, 2));
+        return [newX, newY];
+      })
+    )
+  );
   pop();
-  
-  startAngle = startAngle+1;
+
 }
 
-function windowResized() {
-  setup();
+function windowResized(){
+  resizeCanvas(windowWidth,windowHeight);
 }
-
-function keyPressed(){
-  
-    save('my-great-proejct.png');
-  
-};
