90f1b676b5af46cd974c60c3025941eb7ccbeb95
krstnayou <95864385+krstnayou@users.noreply.github.com>
2023-05-08 07:42:39 -0400

    Add files via upload

--- v3/golden gate/sketch.js
+++ v3/golden gate/sketch.js
@@ -1,74 +1,58 @@
-var gg;
-var widthSlider, rSlider, gSlider, bSlider, oSlider, sSlider;
-var checkbox;
-
-
+let pts, ptss;
+let caslon;
 
 function preload(){
-  gg = loadFont('goldengate.ttf');
-
+  caslon = loadFont('goldengate.otf');
 }
 
 function setup() {
-  createCanvas(windowWidth, 800);
   
-  fill(255);
-  checkbox = createCheckbox('Filled', true);
-  checkbox.changed(myCheckedEvent);
-  checkbox.position(100,700);
+  createCanvas(windowWidth, windowHeight);
   
-  widthSlider = createSlider(0,255,100);
-  widthSlider.position(100,600);
-  rSlider = createSlider(0,255,100);
-  rSlider.position(300,600);
-  gSlider = createSlider(0,255,100);
-  gSlider.position(300,650);
-  bSlider = createSlider(0,255,100);
-  bSlider.position(300,700);
-  oSlider = createSlider(0,255,100);
-  oSlider.position(500,600);
-  sSlider = createSlider(1,12,6);
-  sSlider.position(500,700);
-  
-}
 
+}
 
 function draw() {
+  background(217, 157, 101);
+  translate(mouseX, mouseY);
   
-  const w = widthSlider.value();
-  const r = rSlider.value();
-  const g = gSlider.value();
-  const b = bSlider.value();
-  const o = oSlider.value();
-  const s = sSlider.value();
+    pts = caslon.textToPoints('abcdef ghij', 130, 0, 120,{
+    sampleFactor: 0.25,
+    simplifyThreshold: 1
+  });
+    ptss = caslon.textToPoints('ABCDEFGHIJKLMNOPQRSTU', 0, 10, 120,{
+    sampleFactor: 0.25,
+    simplifyThreshold: 1
+  });
   
   
-  background(0);
-  textSize(w);
-  textFont(gg);
-  fill(r,g,b,o);
-  text("abcdef ghij", width/2-85,352);
-  text("ABCDEFGHIJKLMNOPQRSTU", width/2-200,360);
+  translate(0, 100);
+  beginShape(POINTS);
+  stroke(0,0,0,100);
+  endShape();
   
-  textFont('helvetica');
-  fill(255);
-  textSize(12);
-  text("WIDTH", 100,580);
-  text("FILL/OUTLINE", 100,680);
-  text("COLOR", 300,580);
-  text("OPACITY",500,580);
-  text("STROKE",500,670);
+  translate(0, 100);
+	noFill();
+  let rotX = mouseX/20;
+  let rotY = mouseY/30;
   
-}
-
-
-function myCheckedEvent(){
-  if (checkbox.checked()){
-    fill(100,100,100);
-  } else{
-    noFill();
-    stroke(100,100,100);
-    strokeWeight(s);
-    
+  console.log(rotX);
+  for(let i =0; i< pts.length; i++){      
+    line(pts[i].x, pts[i].y, pts[i].x - rotX, pts[i].y - rotY);
   }
+  
+  for(let i =0; i< ptss.length; i++){      
+    line(ptss[i].x, ptss[i].y, ptss[i].x - rotX, ptss[i].y - rotY);
+  }
+  
+  textFont(caslon);
+  textSize(120);
+  text('abcdef ghij', 130, 0);
+  text('ABCDEFGHIJKLMNOPQRSTU', 0, 10);
+  fill(150,mouseX,100,50);
+  text('abcdef ghij', -rotX+130, -rotY);
+  text('ABCDEFGHIJKLMNOPQRSTU', -rotX, -rotY+10);
+  
+
+  
 }
